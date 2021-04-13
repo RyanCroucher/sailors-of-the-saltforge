f8c69a788a35bdfbe6b07fe78a47bfa998e81ffe
Steven Johnson <sjo139@cs19116bs.canterbury.ac.nz>
2021-04-13 16:44:34 +1200

    Created Item.java and Inventory.java

---
+++ main/Inventory.java
@@ -0,0 +1,95 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ * Class to create inventories, they are handled by a HashMap
+ * @author Steven Johnson sjo139
+ * 
+ */
+public class Inventory {
+
+	private HashMap<Item, Integer> inventory;
+	
+	/**
+	 * Get the quantity of an item given that it is in the HashMap
+	 * @param item The item we want to retrieve the quantity of
+	 * @return The quantity of the given item
+	 */
+	public int getItemQuantity(Item item) {
+		try {
+			int quantity = inventory.get(item);
+			return quantity;
+		} catch (NullPointerException e) {
+			return 0;
+		}
+	}
+	
+	/**
+	 * Set the quantity of an item
+	 * @param item The item to set, given it exists
+	 * @param quantity The quantity to set, must be > 0
+	 */
+	public void setItemQuantity(Item item, int quantity) {
+		if (quantity < 0) {
+			throw new IllegalArgumentException("Quantity should be a minimum of 0");
+		}
+		
+		else if (!inventory.containsKey(item)) {
+			throw new IllegalArgumentException("Item not in inventory");
+		}
+		else {
+			inventory.put(item, quantity);
+		}
+	}
+	
+	/**
+	 * Add quantity of an item into inventory 
+	 * @param item Item to be added
+	 * @param quantity Quantity to be added, must be >=1
+	 */
+	public void addItem(Item item, int quantity) {
+		if (quantity < 1) {
+			throw new IllegalArgumentException("Quantity should be a minimum of 1");
+		}
+		else if (!inventory.containsKey(item)) {
+			inventory.put(item, quantity);
+		}
+		else {
+			inventory.put(item, inventory.get(item) + quantity);
+		}
+	}
+	
+	/**
+	 * Remove quantity of an item from inventory
+	 * @param item Item to remove quantity from. Must exist.
+	 * @param quantity Quantity to remove. Must be >=1 and cannot be greater than current inventory amount.
+	 */
+	public void removeItem(Item item, int quantity) {
+		if (!inventory.containsKey(item)) {
+			throw new IllegalArgumentException("Trying to remove item not in inventory");
+		}
+		else if (quantity < 1) {
+			throw new IllegalArgumentException("Removal quantity should be a minimum of 1");
+		}
+		else if (quantity > inventory.get(item)) {
+			throw new IllegalArgumentException("Cannot remove more items than in inventory");
+		}
+		else {
+			inventory.put(item, inventory.get(item) - quantity);
+		}
+		
+	}
+	
+	/**
+	 * Returns an ArrayList of all items contained in inventory.
+	 * @return ArrayList of items in inventory
+	 */
+	public ArrayList<Item> getInventoryItems() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		items.addAll(inventory.keySet());
+		return items;
+	} 
+	
+	
+}
---
+++ main/Item.java
@@ -0,0 +1,38 @@
+package main;
+/**
+ * Class to create different items, they have a name and basePrice
+ * @author Steven Johnson sjo139
+ *
+ */
+public class Item {
+
+	private String name;
+	private int basePrice;
+	
+	/**
+	 * Construct an item object with name and basePrice parameters
+	 * @param name The name of the item
+	 * @param basePrice The base price of the item before modifiers
+	 */
+	public Item(String name, int basePrice) {
+		this.name = name;
+		this.basePrice = basePrice;
+	}
+	
+	/**
+	 * Get the name
+	 * @return the name of the item
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Get the base price
+	 * @return the base price of the item
+	 */
+	public int getBasePrice() {
+		return basePrice;
+	}
+		
+}
